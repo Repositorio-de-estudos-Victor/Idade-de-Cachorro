726d4604570e00709235f554c30fe8fc88ec2341
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-04-19 00:35:22 -0300

    Recovering knowledge in swift

--- Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let humanAge = Int(campoIdadeCachorro.text!)! * 7
+        
+        legendaResultado.text = "A idade do cachorro em anos humanos é: \(humanAge) "
+    }
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
